7161d4d36c4826ddb3af21c465c168714238d408
Gary Nielsen <nielsen@cheverus.org>
2023-04-14 08:55:08 -0400

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,23 @@
+# you will add your own method here.
+# first add a comment
+# name you method with you initials at the beginning of the method
+# use my code as an example
+
+
+
+
+
+
+
+
+
+
+#   do NOT add any code below this line ! ! ! ! ! ! ! ! ! ! ! ! ! 
+
+# Dr. N.'s hello world method
+def gnMyPrintMethod ():
+  print ("hello from Dr. Nielsen")
+ 
+# we will use this method to print out all our introductions
+def main ():
+  gnMyPrintMethod()
