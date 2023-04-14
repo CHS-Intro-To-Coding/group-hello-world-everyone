a870ef99cf49aeb6518724dab38e7d74d3871c96
LanceChev <130778039+LanceChev@users.noreply.github.com>
2023-04-14 11:41:47 -0400

    Lance's code

--- maincode.py
+++ maincode.py
@@ -3,6 +3,9 @@
 # name you method with you initials at the beginning of the method
 # use my code as an example
 
+#LT's hello world method
+def ltMyPrintMethod():
+  print("hello from Lance Taquet")
 
 
 
