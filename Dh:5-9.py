a07dc1aa6feb3f28e3357b440bf2b95e3de7d9e0
dholmes750 <130777993+dholmes750@users.noreply.github.com>
2023-05-10 08:05:10 -0400

    Update Dh:5-9.py

--- Dh:5-9.py
+++ Dh:5-9.py
@@ -2,5 +2,36 @@ import pygame
 
 pygame.init() 
 
+Wide= 800 
+High=400 
+screenSize = (WIDE,HIGH)  
+WHITE =(255,255,255)
 
-print ('between the init and the quit')
+
+dom= pygame.display.set_mode(screenSize) 
+white = False
+domisworking= True 
+while domisworking: 
+   for event in pygame.event.get(): 
+       if event.type == Pygame.QUIT: 
+          domisworking = False 
+         if event.type == pygame.MOUSEBUTNOTDOM: 
+           if white: 
+              lance.fill(WHITE) 
+           else: 
+            
+              
+         
+   pygame.time.wait(5000)  
+   domisworking = False
+
+
+pygame.quit()
+
+
+
+
+
+
+
+
